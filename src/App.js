12d114a15ba0411d429bc348f236c231e3321eac
Meet Thakkar <meetthakkar05@gmail.com>
2020-11-23 13:12:38 +0530

    Todo App Completed

--- src/App.js
+++ src/App.js
@@ -6,31 +6,56 @@ import TodoList from "./components/TodoList";
 
 class App extends Component {
   state = {
-    items: [
-      { id: 1, title: "Go to bed" },
-      { id: 2, title: "Have Food" },
-      { id: 3, title: "Go to bed" },
-    ],
+    items: [],
     id: uuidv4(),
     item: "",
     editItem: false,
   };
   handleChange = (e) => {
-    console.log(e);
+    this.setState({
+      item: e.target.value,
+    });
   };
   handleSubmit = (e) => {
-    console.log(e);
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    const updatedItems = [...this.state.items, newItem];
+    console.log("123132", updatedItems);
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuidv4(),
+      editItem: false,
+    });
   };
   clearList = () => {
-    console.log("Clear List");
+    this.setState({
+      items: [],
+    });
   };
   handleDelete = (id) => {
-    console.log(`handle Delete ${id}`);
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    this.setState({
+      items: filteredItems,
+    });
   };
   handleEdit = (id) => {
-    console.log(`handle edit ${id}`);
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    const seletedItem = this.state.items.find((item) => item.id === id);
+
+    this.setState({
+      items: filteredItems,
+      item: seletedItem.title,
+      id: id,
+      editItem: true,
+    });
   };
   render() {
+    console.log(this.state.items);
     return (
       <div className="container">
         <div className="row">
@@ -40,7 +65,7 @@ class App extends Component {
               item={this.state.item}
               handleChange={this.handleChange}
               handleSubmit={this.handleSubmit}
-              editItem={this.editItem}
+              editItem={this.state.editItem}
             />
             <TodoList
               items={this.state.items}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,29 @@ import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
+    console.log("78945", items);
     return (
-      <div>
-        <h2>todo List</h2>
-        <TodoItem />
-      </div>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">To Do List</h3>
+        {items?.map((item) => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
+        })}
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-uppercase mt-5"
+          onClick={clearList}
+        >
+          Clear List
+        </button>
+      </ul>
     );
   }
 }
