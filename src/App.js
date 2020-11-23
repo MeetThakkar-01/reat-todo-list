2fb662d2623e497401e727bfdb187c27aa0374d3
Meet Thakkar <meetthakkar05@gmail.com>
2020-11-23 11:29:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
-import TodoItem from "./components/TodoItem";
+
 import TodoList from "./components/TodoList";
 
 function App() {
